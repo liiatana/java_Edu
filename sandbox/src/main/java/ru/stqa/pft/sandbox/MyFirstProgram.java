22ecb22891ae606d6c21c70a3916bf48a902f424
liiatana <31036379+liiatana@users.noreply.github.com>
2017-08-25 18:30:45 +0300

    Задание 2. Добавлен метод вычисления расстояния в класс Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,12 +6,14 @@ public class MyFirstProgram {
     Point p1 = new Point(2, 3);
     Point p2 = new Point(6, 8);
 
-    System.out.println("Расстояние между точками с координатами:(" + p1.x + "," + p2.x + ") и (" + p2.x + "," + p2.y + ") составляет " + distance(p1, p2));
+     System.out.println("Расстояние между точками с координатами:(" + p1.x + "," + p2.x + ") и (" + p2.x + "," + p2.y + ") составляет " + distance(p1, p2));
 
-
+    p2.distance(p1, p2);
   }
 
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2));
-  }
+   public static double distance(Point p1, Point p2) {
+     return Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2));
+   }
+
+
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -4,8 +4,14 @@ public class Point {
   public double x;
   public double y;
 
-  public Point(double x,double y){
-    this.x=x;
-    this.y=y;
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public static void distance(Point p1, Point p2) {
+    double result = Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p2.y - p1.y, 2));
+    System.out.println("Расстояние между точками с координатами:(" + p1.x + "," + p2.x + ") и (" + p2.x + "," + p2.y + ") составляет " + result);
+
   }
 }
