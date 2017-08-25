b5d216ea725e22cdf91bcae0fb4452280ab0a2d5
liiatana <31036379+liiatana@users.noreply.github.com>
2017-08-25 13:11:18 +0300

    Добавлены фунции в MyFirstProgram

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,34 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-	}
+  public static void main(String[] args) {
+    //String some="tester";
+    //System.out.println("Hello,"+some+ "!");
+    hello("Tester");
+    hello("world");
+    double l = 6.2;
+    double s = area(l);
+    System.out.println("Площадь квадрата со стороной " + l + " равна " + s);
+
+    double a = 6.2;
+    double b = 6.4;
+    System.out.println("Площадь прямоугольника со сторонами: " +a +" и "+ b+ " равна " + area(a,b));
+  }
+
+  public static void hello(String some) {
+
+    System.out.println("Hello," + some + "!");
+    //System.out.println("Hello,world!");
+  }
+
+  public static double area(double l) {
+    return l * l;
+  }
+
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
+
+
 }
