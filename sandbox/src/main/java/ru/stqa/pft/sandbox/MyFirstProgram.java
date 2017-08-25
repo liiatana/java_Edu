cb63e58b5034bc6c196a427c9691efb219369bbc
liiatana <31036379+liiatana@users.noreply.github.com>
2017-08-25 13:41:12 +0300

    Добавлены классы вычисления площадей

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,13 +6,16 @@ public class MyFirstProgram {
     //System.out.println("Hello,"+some+ "!");
     hello("Tester");
     hello("world");
-    double l = 6.2;
-    double s = area(l);
-    System.out.println("Площадь квадрата со стороной " + l + " равна " + s);
 
-    double a = 6.2;
-    double b = 6.4;
-    System.out.println("Площадь прямоугольника со сторонами: " +a +" и "+ b+ " равна " + area(a,b));
+    Square s=new Square(6.9);
+    //s.l = 6.2;
+    double s1 = area(s);
+    System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s1);
+
+    Rectangle r=new Rectangle(7.3,8.0);
+   // r.a = 6.2;
+    //r.b = 6.7;
+    System.out.println("Площадь прямоугольника со сторонами: " +r.a +" и "+ r.b+ " равна " + area(r));
   }
 
   public static void hello(String some) {
@@ -21,13 +24,13 @@ public class MyFirstProgram {
     //System.out.println("Hello,world!");
   }
 
-  public static double area(double l) {
-    return l * l;
+  public static double area(Square s) {
+    return s.l * s.l;
   }
 
 
-  public static double area(double a, double b) {
-    return a * b;
+  public static double area(Rectangle r) {
+    return r.a * r.b;
   }
 
 
