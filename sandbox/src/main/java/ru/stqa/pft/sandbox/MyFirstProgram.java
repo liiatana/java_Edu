7aec48ba33a531c188f8d87cead5cd1b90a40523
liiatana <31036379+liiatana@users.noreply.github.com>
2017-08-25 18:17:38 +0300

    Задание 2. Вычисление расстояние функцией
    Также убраны классы Rectange + Square

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,27 +2,16 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    //String some="tester";
-    //System.out.println("Hello,"+some+ "!");
-    hello("Tester");
-    hello("world");
 
-    Square s=new Square(6.9);
-    double s1 = s.area();
-    System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s1);
+    Point p1 = new Point(2, 3);
+    Point p2 = new Point(6, 8);
 
-    Rectangle r=new Rectangle(7.3,8.0);
-       System.out.println("Площадь прямоугольника со сторонами: " +r.a +" и "+ r.b+ " равна " + r.area());
+    System.out.println("Расстояние между точками с координатами:(" + p1.x + "," + p2.x + ") и (" + p2.x + "," + p2.y + ") составляет " + distance(p1, p2));
+
+
   }
 
-  public static void hello(String some) {
-
-    System.out.println("Hello," + some + "!");
-    //System.out.println("Hello,world!");
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2));
   }
-
-  public static double distance(Point p1, Point p2){
-    return Math.sqrt(Math.pow((p1.x-p2.x), 2)-Math.pow((p1.y-p2.y), 2));
-  }
-
 }
