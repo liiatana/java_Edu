5dd4b05e2271ca1c2633d115a558b6ad02fabac0
liiatana <31036379+liiatana@users.noreply.github.com>
2017-08-29 22:36:15 +0300

    Правильное сравнение строк(equls вместо двойного равенства)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.util.Objects;
+
 public class ApplicationManager {
 
   WebDriver wd;
@@ -26,16 +28,16 @@ public class ApplicationManager {
 
     //String browser = BrowserType.FIREFOX;
 
-    if (browser == BrowserType.FIREFOX) {
+    if (Objects.equals(browser, BrowserType.FIREFOX)) {
       wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-    } else if (browser == BrowserType.CHROME) {
+    } else if (Objects.equals(browser, BrowserType.CHROME)) {
       wd = new ChromeDriver();
-    } else if ((browser == BrowserType.IE)) {
+    } else if ((Objects.equals(browser, BrowserType.IE))) {
       wd = new InternetExplorerDriver();
     }
 
 
-//    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    //wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost:8080/addressbook/");
     groupHelper = new GroupHelper(wd);
     navigationManager = new NavigationManager(wd);
