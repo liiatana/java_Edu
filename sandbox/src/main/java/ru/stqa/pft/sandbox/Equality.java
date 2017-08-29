bb1acb8e7dca91a7a69606eb607b9d289d6e5a16
liiatana <31036379+liiatana@users.noreply.github.com>
2017-08-29 22:49:36 +0300

    Equals Vs Двойное равенство.

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equality.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+public class Equality {
+
+  public static void main(String[] args) {
+
+    String s1 = "firefox 2.0";
+    //String s2=s1;
+    //String s2 = new String(s1);
+    //String s2="firefox";
+    //String s2="fire"+"fox";
+    String s2="firefox "+ Math.sqrt(4.0);
+
+    System.out.println(s1 == s2);
+    System.out.println(s1.equals(s2));
+
+  }
+}
