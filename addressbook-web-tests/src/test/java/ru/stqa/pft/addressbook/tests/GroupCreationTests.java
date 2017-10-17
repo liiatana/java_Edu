a99a3cdc672cd4fc912a6aca9584c00b0591a569
liiatana <31036379+liiatana@users.noreply.github.com>
2017-10-17 12:21:09 +0300

    4.9 Модификация теста создания групп.Определения макс.идентификатора через лямбда выражения

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,7 +15,7 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
 
     GroupData newGroup = new GroupData("CreateGr", "test2", null);
-    int maxId = 0;
+
 
     app.getNavigationManager().gotoGroupPage();
     //int before= app.getGroupHelper().getGroupCount();
@@ -30,15 +31,20 @@ public class GroupCreationTests extends TestBase {
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    for (GroupData id : after) {
+    /*
+    int maxId = 0;
+    for (GroupData id : after) {//1ый способ поиска максимального элемента
       if (id.getId() > maxId) {
         maxId = id.getId();
       }
       ;
     }
-    ;
+    ;*/
 
-    newGroup.setId(maxId);
+    /*Comparator<? super GroupData> byId= (Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(),o2.getId());//2-3ый способ поиска максимального элемента(только с java8)
+    maxId = after.stream().max(byId).get().getId();*/
+
+    newGroup.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
     before.add(newGroup);
     Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));//HashSet  создает неупорядоченное множество. преобразут наши оба списка в множества
 
