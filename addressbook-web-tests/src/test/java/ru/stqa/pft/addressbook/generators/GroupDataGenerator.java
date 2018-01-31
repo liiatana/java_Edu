43f4341a832c02eaa8c4f6e663f65cea8dc80400
liiatana <31036379+liiatana@users.noreply.github.com>
2018-01-31 19:02:39 +0300

    Лекция 6.3. Анализ опций командой JCommander

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import gw.internal.ext.com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
@@ -10,16 +13,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GroupDataGenerator {
+
+  @Parameter(names = "-c", description="Groups count")
+  public int count;
+
+  @Parameter(names = "-f", description="File for export")
+  public String file;
+
+
   public static void main(String[] args) throws IOException {
-    int count=Integer.parseInt(args[0]);
-    File file=new File(args[1]);
-    
-    List<GroupData> groups=generateGroups(count);
-    save(groups,file);
+    GroupDataGenerator generator= new GroupDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+        jCommander.parse(args);
+    } catch (ParameterException ex){
+        jCommander.usage();
+        return;
+
+    }
+
+    generator.run();
+
 
   }
 
-  private static void save(List<GroupData> groups, File file) throws IOException {
+  private void run() throws IOException {
+    List<GroupData> groups=generateGroups(count);
+    save(groups,new File(file));
+
+  }
+
+  private void save(List<GroupData> groups, File file) throws IOException {
 
     Writer writer=new FileWriter(file);
     for(GroupData group:groups){
@@ -30,7 +54,7 @@ public class GroupDataGenerator {
 
   }
 
-  private static List<GroupData> generateGroups(int count) {
+  private List<GroupData> generateGroups(int count) {
 
     List<GroupData> groups= new ArrayList<GroupData>();
     for( int i=0; i<count;i++){
