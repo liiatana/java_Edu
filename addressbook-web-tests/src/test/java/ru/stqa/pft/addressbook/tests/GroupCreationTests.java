55ca4af28a80162078d6409bfd07ded2dab09247
liiatana <31036379+liiatana@users.noreply.github.com>
2018-02-01 10:01:29 +0300

    Лекция 6.4. Параметризация тестовых методов
    на примере групп( передача строк как входных параметров теста)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -6,22 +6,37 @@ import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
+  @DataProvider
+  public java.util.Iterator <Object[]> validGroups(){
+    List<Object[]> list=new ArrayList< Object[]>();
+    list.add(new Object[] {"test1","h1","f1"});
+    list.add(new Object[] {"test2","h2","f2"});
+    list.add(new Object[] {"test3","h3","f3"});
+    return list.iterator();
+
+  }
+
+  @Test(dataProvider = "validGroups")
   //создание группы в адресной книге
-  public void testGroupCreation() {
+  public void testGroupCreation( String name,String header,String footer) {
 
 
     app. goTo().groupPage();
     Groups before = app.group().all();
 
     GroupData newGroup = new GroupData()
-            .withName ("CreateGr");
+            .withName (name)
+            .withHeader(header)
+            .withFooter(footer);
 
     app.group().create(newGroup);
     assertThat(app.group().count(), equalTo(before.size()+1 ));
